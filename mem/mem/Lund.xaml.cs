dd3ae4ea3caa2f21d07afb60a650ee3578a117fa
Opilane TTHK <opilane@tthk.ee>
2020-09-25 12:09:33 +0300

    Add project files.

---
+++ mem/mem/Lund.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace mem
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Lund : ContentPage
+    {
+        public Lund()
+        {
+            AbsoluteLayout abs = new AbsoluteLayout();
+            pea = new BoxView { Color = Color.LightBlue, CornerRadius = 50 };
+            AbsoluteLayout.SetLayoutFlags(pea, AbsoluteLayoutFlags.All);
+            abs.Children.Add(pea);
+            Content = abs;
+        }
+    }
+}
